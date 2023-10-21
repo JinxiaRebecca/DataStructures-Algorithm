a02bd7de5f6f3fc5ae296a4ac8f9220559a3f998
JinxiaRebecca <18502908857@163.com>
2023-10-21 11:07:03 +0800

    leecode-no.100 反转链表

---
+++ datastructures/src/com/demo/leecode/linkedList/N100.java
@@ -0,0 +1,69 @@
+package com.demo.leecode.linkedList;
+
+/**
+ * @author Rebecca Jin
+ * @date 2023/10/21 10:58
+ * <p>
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * <p>
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 示例 3：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ */
+public class N100 {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4  = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode newHead = reverseList(node1);
+        while (newHead != null) {
+            System.out.println(newHead.val);
+            newHead = newHead.next;
+        }
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = head;
+        ListNode current = head.next;
+        prev.next = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
